98ff7458ec196b5af5ca75b09d94aec3af55a1ee
ClydeDroid <clydedroid@gmail.com>
2016-12-16 14:24:10 -0500

    Edit dir file and add new file in dir

--- octokit_test.rb
+++ octokit_test.rb
@@ -1,35 +1,63 @@
 # Thanks to Matt Greensmith! http://mattgreensmith.net/2013/08/08/commit-directly-to-github-via-api-with-octokit/
 require 'octokit'
 require 'base64'
+require 'dotenv'
 
 # Setup
-octokit = Octokit::Client.new(access_token: ENV['GITHUB_API_TOKEN'])
-repo = 'ClydeDroid/octokit-testing'
+Dotenv.load
+octokit = Octokit::Client.new(access_token: ENV['GITHUB_ACCESS_TOKEN'])
+repo = "#{ENV['GITHUB_USERNAME']}/octokit-testing"
 master_ref = 'heads/master'
 latest_commit_sha = octokit.ref(repo, master_ref).object.sha
 base_tree_sha = octokit.commit(repo, latest_commit_sha).commit.tree.sha
 
 # Get base text file and edit
-base_file = octokit.content(repo, path: 'test.txt')
-base_content = Base64.decode64(base_file.content)
-edit_count = base_content.scan(/\d/)[0].to_i + 1
-new_base_content = base_content.gsub(/\d/, edit_count.to_s)
+file = octokit.content(repo, path: 'test.txt')
+content = Base64.decode64(file.content)
+edit_count = content.scan(/\d/)[0].to_i + 1
+new_content = content.gsub(/\d/, edit_count.to_s)
+base_blob_sha = octokit.create_blob(repo, Base64.encode64(new_content), 'base64')
 
-# Create git objects
-blob_sha = octokit.create_blob(repo, Base64.encode64(new_base_content), 'base64')
+# Get text file in dir and edit
+file = octokit.content(repo, path: 'dir/original-test.txt')
+content = Base64.decode64(file.content)
+edit_count = content.scan(/\d/)[0].to_i + 1
+new_content = content.gsub(/\d/, edit_count.to_s)
+dir_blob_sha = octokit.create_blob(repo, Base64.encode64(new_content), 'base64')
+
+# Create new file in dir
+content = "I am new file ##{edit_count}!"
+new_file_path = "dir/test-#{edit_count}.txt"
+new_blob_sha = octokit.create_blob(repo, Base64.encode64(content), 'base64')
+
+# Create tree objects
 new_tree_sha = octokit.create_tree(
   repo,
-  [{
-    path: 'test.txt',
-    mode: '100644',
-    type: 'blob',
-    sha: blob_sha
-  }],
+  [
+    {
+      path: 'test.txt',
+      mode: '100644',
+      type: 'blob',
+      sha: base_blob_sha
+    },
+    {
+      path: 'dir/original-test.txt',
+      mode: '100644',
+      type: 'blob',
+      sha: dir_blob_sha
+    },
+    {
+      path: new_file_path,
+      mode: '100644',
+      type: 'blob',
+      sha: new_blob_sha
+    }
+  ],
   base_tree: base_tree_sha
 ).sha
 
 # Commit to master on GitHub
-commit_message = 'Edit test.txt and commit via Octokit!'
+commit_message = "Commit via Octokit! (Edit ##{edit_count})"
 new_commit_sha = octokit.create_commit(
   repo,
   commit_message,
